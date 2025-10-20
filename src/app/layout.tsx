6f96e0b11d6227620583748a9ca65d82648371d1
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-10-19 19:39:50 -0600

    google analytics were in the body, perhaps that was the issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import { CookieConsent } from "@/components/CookieConsent";
 import { AuthProvider } from "@/components/AuthProvider";
 import { LazyMotion, domAnimation } from "framer-motion";
-import ManualGoogleAnalytics from "@/components/GoogleAnalytics";
+// import ManualGoogleAnalytics from "@/components/GoogleAnalytics";
 
 
 const geistSans = Geist({
@@ -101,10 +101,10 @@ export default function RootLayout({
         </LazyMotion>
         <Analytics />
         <SpeedInsights />
-        {/* <GoogleAnalytics gaId="G-RPDGXS3WCB" /> */}
-        <ManualGoogleAnalytics GA_TRACKING_ID="G-RPDGXS3WCB" />
         <CookieConsent />
       </body>
+        {/* <ManualGoogleAnalytics GA_TRACKING_ID="G-RPDGXS3WCB" /> */}
+      <GoogleAnalytics gaId={"G-RPDGXS3WCB"} />
     </html>
   );
 }
